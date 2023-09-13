c286a9f34c230498b4568d8274195ce55f129eeb
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-09-13 16:32:59 +0530

    Update clock cycles

--- src/test.py
+++ src/test.py
@@ -14,7 +14,7 @@ async def test_ti_sbox(dut):
     dut._log.info("reset")
     dut.rst_n.value = 0
     dut.ena.value = 0
-    await ClockCycles(dut.clk, 1100)
+    await ClockCycles(dut.clk, 2200)
     dut.rst_n.value = 1
     dut.ena.value = 1
     dut.ui_in.value = input[0]
