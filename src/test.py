0cfdd32f3939d87fc41baa67995fe3989ebc336a
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-09-28 15:23:10 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -8,35 +8,35 @@ output = [0x0, 0x0, 0x55, 0xa2, 0x0c]
 @cocotb.test()
 async def test_ti_sbox(dut):
     dut._log.info("start")
-    clock = Clock(dut.clk, 10, units="ns")
+    clock = Clock(dut.clk, 20, units="ns")
     cocotb.start_soon(clock.start())
 
     dut._log.info("reset")
     dut.rst_n.value = 0
     dut.ena.value = 0
-    await ClockCycles(dut.clk, 6)
+    await ClockCycles(dut.clk, 2)
     dut.rst_n.value = 1
     dut.ena.value = 1
     dut.ui_in.value = input[0]
     dut._log.info("output at reset low = {}".format(int(dut.uo_out.value)))
     dut._log.info("output ready = {}".format(int(dut.uio_out.value)))
-    await ClockCycles(dut.clk, 20)
+    await ClockCycles(dut.clk, 1)
     dut.ui_in.value = input[1]
     dut._log.info("output during input buffering = {}".format(int(dut.uo_out.value)))
     dut._log.info("output ready = {}".format(int(dut.uio_out.value)))
-    await ClockCycles(dut.clk, 20)
+    await ClockCycles(dut.clk, 1)
     dut.ui_in.value = input[2]
     dut._log.info("output during input buffering = {}".format(int(dut.uo_out.value)))
     dut._log.info("output ready = {}".format(int(dut.uio_out.value)))
-    await ClockCycles(dut.clk, 20)
+    await ClockCycles(dut.clk, 1)
     dut.ui_in.value = input[3]
     dut._log.info("output during input buffering = {}".format(int(dut.uo_out.value)))
     dut._log.info("output ready = {}".format(int(dut.uio_out.value)))
-    await ClockCycles(dut.clk, 20)
+    await ClockCycles(dut.clk, 1)
     dut.ui_in.value = input[4]
     dut._log.info("output during input buffering = {}".format(int(dut.uo_out.value)))
     dut._log.info("output ready = {}".format(int(dut.uio_out.value)))
-    await ClockCycles(dut.clk, 20)
+    await ClockCycles(dut.clk, 1)
     dut.ena.value = 0
     dut._log.info("output at ena high = {}".format(int(dut.uo_out.value)))
     dut._log.info("output ready = {}".format(int(dut.uio_out.value)))
@@ -54,4 +54,4 @@ async def test_ti_sbox(dut):
         # assert int(dut.uo_out.value) == output[i]
         assert dut.uio_oe == 0xFF
         # assert dut.uio_out == 0x01
-        await ClockCycles(dut.clk, 20)
+        await ClockCycles(dut.clk, 1)
