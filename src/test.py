5cbef41c3ac51194c595ca9e4c5c2444b94fd99f
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-09-28 15:37:03 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -14,10 +14,10 @@ async def test_ti_sbox(dut):
     dut._log.info("reset")
     dut.rst_n.value = 0
     dut.ena.value = 0
-    await ClockCycles(dut.clk, 2)
+    dut.ui_in.value = input[0]
+    await ClockCycles(dut.clk, 3)
     dut.rst_n.value = 1
     dut.ena.value = 1
-    dut.ui_in.value = input[0]
     dut._log.info("output at reset low = {}".format(int(dut.uo_out.value)))
     dut._log.info("output ready = {}".format(int(dut.uio_out.value)))
     await ClockCycles(dut.clk, 1)
